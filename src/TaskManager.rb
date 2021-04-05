04d4ffc24e2d0d00f922149730bbcd89a5124e71
dean <deanraguso@gmail.com>
2021-04-05 17:18:21 +1000

    create task method now uses config file to create a different unique id each time, using persistent config.

--- src/TaskManager.rb
+++ src/TaskManager.rb
@@ -4,18 +4,22 @@ class TaskManager
     def initialize
         # Database loads into array db
         @db_address = ""
+        @db_config_address = ""
         @db = []
 
         # Load session files
         load_config
-        load_db
 
-        new_task
+        # Load in DB
+        load_db
     end
 
     def load_config
         settings_file = File.open("./config/settings.txt","r")
-        @db_address = settings_file.read
+        config = settings_file.readlines()
+
+        @db_address = config[0].split(":")[1].chomp
+        @db_config_address = config[1].split(":")[1].chomp
     end 
 
     def save_task(task)
@@ -29,6 +33,19 @@ class TaskManager
         @db = File.readlines(@db_address)
     end
 
+    def get_next_id
+        # Get the Unique next task ID
+        next_id = File.read(@db_config_address).split(":")[1].to_i
+        puts next_id
+
+        #Increment the task ID so the next retrieval is also unique
+        db_config_fp = File.open(@db_config_address, "w")
+        db_config_fp.write("Next Unique Task ID:#{(next_id + 1)}")
+        db_config_fp.close
+
+        return next_id
+    end
+
     def load_task
         t = Task.new(0)
         t.load_from_s(@db_fp.read)
@@ -37,7 +54,7 @@ class TaskManager
     end
 
     def new_task
-        t = Task.new(1) #Must change number
+        t = Task.new(get_next_id) #Must change number
         t.create
         save_task(t)
     end
