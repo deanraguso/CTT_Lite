8ff50062fbccf642444227e77e1d9db8e467b939
dean <deanraguso@gmail.com>
2021-04-06 11:31:14 +1000

    refactored form_schedule method

--- src/Schedule.rb
+++ src/Schedule.rb
@@ -31,35 +31,28 @@ class Schedule
 
     # Must adjust so that increment keeps happening to skip over rest_days
     def form_schedule(db, timeframe=7)
-        if sufficient_time?(timeframe)
-            index = 0;
+        index = 0
 
-            db.each do |task|
-                if ( task.time_required + @plan[index][:hours] )*@safety_factor <= @working_hours
+        db.each do |task|
+            if ( task.time_required + @plan[index][:hours] ) * @safety_factor <= @working_hours
 
-                    # If the plan fits on the current day
-                    @plan[index][:hours] += task.time_required
-                    @plan[index][:tasks] << task
-                else 
-                    #If the plan didn't fit, go to next day
-                    index += 1 #Adjust this to account for weekends later
+                # If the plan fits on the current day
+                @plan[index][:hours] += task.time_required
+                @plan[index][:tasks] << task
+                puts "Iterating over id:#{task.id}, day #{index + 1} total #{@plan[index][:hours]}"
 
-                    @plan[index] = Hash.new
-                    @plan[index][:hours] = 0.0
-                    @plan[index][:tasks] = []
+            else 
+                #If the plan didn't fit, go to next day
+                index += 1 #Adjust this to account for weekends later
 
-                    @plan[index][:hours] += task.time_required
-                    @plan[index][:tasks] << task
-                end
+                @plan[index] = Hash.new
+                @plan[index][:hours] = 0.0
+                @plan[index][:tasks] = []
+                
+                @plan[index][:hours] += task.time_required
+                puts "Iterating over id:#{task.id}, day #{index + 1} total #{@plan[index][:hours]}"
+                @plan[index][:tasks] << task
             end
-
-            if index >= timeframe
-                puts "Couldn't fit all tasks comfortably in #{timeframe} days."
-            end
-
-        else
-            puts "There is insufficient time in a #{timeframe} day 
-            window to complete the tasks on your schedule!"
         end
     end
 
